70d7748eba03e2ef1a128918a57d246e75a07a99
antoniocra04 <antoniocra04@gmail.com>
2020-06-15 20:18:36 +0600

    Updates

---
+++ precache-manifest.f7afe307973a302c9fb59b96ed6dd812.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77daa507b2a8be1a389ac66c4a390b6",
+    "url": "/inSecure/index.html"
+  },
+  {
+    "revision": "1662542e14cfaac1273f",
+    "url": "/inSecure/static/css/main.a26b5ff8.chunk.css"
+  },
+  {
+    "revision": "a1623ac0c6852c55cd84",
+    "url": "/inSecure/static/js/2.7fb801a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inSecure/static/js/2.7fb801a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1662542e14cfaac1273f",
+    "url": "/inSecure/static/js/main.a100f5fc.chunk.js"
+  },
+  {
+    "revision": "f434d1e2efcbc5ffbb8b",
+    "url": "/inSecure/static/js/runtime-main.335b8d5b.js"
+  },
+  {
+    "revision": "226d519086a3835e16c87f4ab1cd5641",
+    "url": "/inSecure/static/media/GothamPro-Black.226d5190.woff2"
+  },
+  {
+    "revision": "ad1867c1adc22645240b3fd33a205f9e",
+    "url": "/inSecure/static/media/GothamPro-Bold.ad1867c1.woff2"
+  },
+  {
+    "revision": "964e1508f92be294b2d5be7e8ef64192",
+    "url": "/inSecure/static/media/GothamPro-Light.964e1508.woff2"
+  },
+  {
+    "revision": "032df678e57acf963dcfc62625bb61e0",
+    "url": "/inSecure/static/media/GothamPro-Medium.032df678.woff2"
+  },
+  {
+    "revision": "f02e0f6b007b64a0567060a299cedf4e",
+    "url": "/inSecure/static/media/GothamPro-Reg.f02e0f6b.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inSecure/static/media/logo.5d5d9eef.svg"
+  }
+]);
